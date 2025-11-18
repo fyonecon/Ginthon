1112faef034ada4f523a6f849f84b64d53886e7f
fyonecon <fyonecon@gmail.com>
2025-11-18 14:21:59 +0800

    update

---
+++ bootstrap/close_app.py
@@ -0,0 +1,5 @@
+# 处理关闭程序时需要打扫的事件
+
+def close_app():
+
+    return
---
+++ common/txt_data.py
@@ -0,0 +1,18 @@
+
+# 读写txt文件
+
+def write_txt():
+
+    return
+
+def read_txt():
+
+    return
+
+def create_txt():
+
+    return
+
+def del_txt():
+
+    return 
