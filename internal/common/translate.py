aeaa7063ad9f03282fcd6674dfff0a69269faab2
fyonecon <fyonecon@gmail.com>
2025-11-28 18:05:10 +0800

    增加翻译

--- internal/common/translate.py
+++ internal/common/translate.py
@@ -5,21 +5,21 @@ def get_translate(key="-null-", lang=""):
     # 将语言转换成可用的数组索引标记
     def make_lang_index(language):
         _language = language.lower()
-        if "zh" in _language:  # 简体中文（包含繁体）
-            return "zh",
-        elif "en" in _language:  # 英文
+        if _language.find("zh", 0)==0 or _language.find("chinese", 0)==0:  # 简体中文（包含繁体）
+            return "zh"
+        elif _language.find("en", 0)==0 or _language.find("english", 0)==0:  # 英文
             return "en"
-        elif "jp" in _language:  # 日文
+        elif _language.find("jp", 0)==0:  # 日文
             return "jp"
-        elif "fr" in _language:  # 法语
+        elif _language.find("fr", 0)==0:  # 法语
             return "fr"
-        elif "de" in _language:  # 德语
+        elif _language.find("de", 0)==0:  # 德语
             return "de"
-        elif "ru" in _language:  # 俄语或乌克兰语
+        elif _language.find("ru", 0)==0:  # 俄语或乌克兰语
             return "ru"
-        elif "es" in _language:  # 西班牙语
+        elif _language.find("es", 0)==0:  # 西班牙语
             return "es"
-        elif "vi" in _language:  # 越语
+        elif _language.find("vi", 0)==0:  # 越语
             return "vi"
         else:  # 默认英文
             return "en"
@@ -31,21 +31,24 @@ def get_translate(key="-null-", lang=""):
             return locale.getlocale()[0]
     #
     lang_index = make_lang_index(sys_language(lang))
-    has_key = lang_dict.get(key)
-    has_lang = lang_dict.get(key).get(lang_index)
     #
-    if has_key and has_lang:
-        txt = lang_dict[key][lang_index]
-        if len(txt)>0:
+    if lang_dict.get(key):
+        if lang_dict[key].get(lang_index):
             return lang_dict[key][lang_index]
-        else: # 为空就默认显示英文
-            return " -" + key + "📍" + lang_index + "- "
-    else: # 还没有定义key
-        return lang_dict["-null-"]["en"]
+        else:
+            if len(lang_dict[key]["en"])>=1:
+                return lang_dict[key]["en"]
+            else:
+                return lang_dict["-null-"]["en1"]
+    else:
+        if lang_dict["-null-"].get(lang_index):
+            return lang_dict["-null-"][lang_index]
+        else:
+            return lang_dict["-null-"]["en2"]
 
 # 翻译表
 lang_dict = {
-"test": {
+"test": { # 示例
     "zh": "测试",
     "en": "Test",
     "jp": "",
@@ -55,15 +58,11 @@ lang_dict = {
     "es": "",
     "vi": "",
 },
-"-null-":{
+"-null-":{ # 必须
     "zh": " -空- ",
     "en": " -Null- ",
-    "jp": " -Null- ",
-    "fr": " -Null- ",
-    "de": " -Null- ",
-    "ru": " -Null- ",
-    "es": " -Null- ",
-    "vi": " -Null- ",
+    "en1": " -Null-1- ",
+    "en2": " -Null-2- ",
 },
 #
 "show_window": {
