970adc7762a13f29d4c2098e92351e3f6c14393f
fyonecon <fyonecon@gmail.com>
2025-11-30 12:07:11 +0800

    v1.2.3

--- internal/app/flask/controller/window_view.py
+++ internal/app/flask/controller/window_view.py
@@ -14,11 +14,12 @@ def window_view(rand_id, filename):
         return content
     #
     view_url = "http://127.0.0.1:9100/view"
+    view_html = view_url + "/" + filename
     file_path = mian_virtual_dirpath("frontend") + "/view/"+filename
     #
     html = read_html(file_path)
     js = f'''
-    <script>const view_url = "{view_url}";</script>
+    <script>const view_url = "{view_url}";const view_html="{view_html}"; const view_filename = "{filename}";</script>
     '''
     if len(html)==0:
         html = '''
@@ -26,10 +27,52 @@ def window_view(rand_id, filename):
         <head>
             <meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=0" />
             <title>Default Window</title>
+            <style>
+                .hide{
+                    display: none !important;
+                }
+                .click{
+                    cursor: pointer;
+                }
+                .click:active{
+                    opacity: 0.6;
+                }
+                .select-none{
+                    -moz-user-select: none;-webkit-user-select: none;-ms-user-select: none;
+                    user-select: none;
+                }
+                .break{
+                    overflow: hidden;
+                    word-wrap: break-word;
+                    overflow-wrap: break-word;
+                }
+            </style>
         </head>
         <body style="background-color: transparent;">
-            <h2>当前使用了空模板。</h2>
-            <img src="http://127.0.0.1:9100/file/test.png" width="192" alt=""/>
+            <br/>
+            <h2 style="text-align: center; " class="select-none">当前使用了空模板。</h2>
+            <div style="text-align: center;">
+                <p id="info" class="break"></p>
+                <p class="select-none"><img src="http://127.0.0.1:9100/file/test.png" width="192" alt=""/></p>
+            </div>
+            <script>
+            function show_info(_view_html) {
+                let info = [
+                    window.location.host, 
+                    !!window.localStorage, 
+                    !!window.indexedDB, 
+                    navigator.webdriver, 
+                    navigator.languages, 
+                    window.matchMedia("(prefers-color-scheme: dark)").matches?"dark":"light", "✅", 
+                    window.navigator.userAgent,
+                    view_url,
+                    view_html,
+                ]; 
+                console.log(info);
+                document.getElementById("info").innerHTML = view_filename+" 文件不存在。"; 
+            }
+            show_info(view_html);
+            </script>
         </body>
         </html>
         '''
